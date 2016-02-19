ca9953670e8d905b194b1f16b83681d9677d7ba0
Cristian Pereyra <cristian@redmintlabs.com>
2016-02-19 02:56:06 -0300

    update start animations

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+  $(".js-check-it-out").on("click", function() {
+    $(".fullscreen").addClass("modify");
+    $(window).scrollTo('.work', 500, { offset: -180 });
+  });
 
+  var wHeight = $( window ).height();
+  var isMobile = $( window ).width() < 768;
+
+  $('.work').waypoint({
+    handler: function(direction) {
+      if (direction == 'down') {
+        $(".fullscreen").addClass("modify");
+      } else {
+        $(".fullscreen").removeClass("modify");
+      }
+    },
+    offset: (isMobile) ? 0 : (5 * wHeight / 6) + 'px'
+  })
+});
